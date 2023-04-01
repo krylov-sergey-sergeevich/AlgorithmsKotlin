7725d2d2ca5842a28f149e4716fe39f091eb9a49
Krylov Sergey <krylov.sergey.sergeevich@gmail.com>
2023-04-01 16:25:21 +0300

    Реализованы базовые алгоритмы 'Arrays'

--- algo/src/main/kotlin/ru/kssergey/algo/Arrays.kt
+++ algo/src/main/kotlin/ru/kssergey/algo/Arrays.kt
@@ -8,11 +8,48 @@ class Arrays {
      * @param arr массив элементов
      * @return максимальное значение
      */
-    fun getMaxElement(vararg arr: Long): Long {
-        var result = arr[0]
+    fun getMaxElement(arr: LongArray): Long {
+        var result = Long.MIN_VALUE
         for (el in arr) {
             result = maxOf(el, result)
         }
+        return result
+    }
+
+    /**
+     * Нахождение максимального элемента в массиве, который меньше topBoundary.
+     *
+     * @param arr массив элементов
+     * @param topBoundary параметр ограничивающий максимальное значение
+     * @return максимальное значение
+     */
+    fun findMaxUnderBoundary(arr: LongArray, topBoundary: Long): Long {
+        var result = Long.MIN_VALUE
+        for (el in arr) {
+            if (el < topBoundary) {
+                result = maxOf(el, result)
+            }
+        }
+        return result
+    }
+
+    /**
+     * Нахождение N максимальных элементов в массиве / Сортировка выбором.
+     *
+     * @param arr массив элементов
+     * @param numberOfElements кол-во элементов
+     * @return список максимальных элементов
+     */
+    fun findTopElements(arr: LongArray, numberOfElements: Int): List<Long> {
+        val result = mutableListOf<Long>()
+
+        var previousMax = Long.MAX_VALUE
+        for (el in 1..numberOfElements) {
+            val currentMax = findMaxUnderBoundary(arr, previousMax)
+            result.add(currentMax)
+            previousMax = currentMax
+        }
+
         return result
     }
 }
--- algo/src/test/kotlin/ru/kssergey/algo/ArraysTest.kt
+++ algo/src/test/kotlin/ru/kssergey/algo/ArraysTest.kt
@@ -1,18 +1,26 @@
 package ru.kssergey.algo
 
+import lombok.extern.slf4j.Slf4j
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-/**
- * @author Krylov Sergey
- * @date  01.04.2023 15:50
- */
+@Slf4j
 class ArraysTest {
     private val arraysAlgo = Arrays()
 
     @Test
     fun testArrayMaxElement() {
-        assertEquals(10, arraysAlgo.getMaxElement(1, 2, 3, 4, 5, 10))
-        assertEquals(0, arraysAlgo.getMaxElement(-5, -3, -1, 0))
+        assertEquals(10, arraysAlgo.getMaxElement(longArrayOf(1, 2, 3, 4, 5, 10)))
+        assertEquals(0, arraysAlgo.getMaxElement(longArrayOf(-5, -3, -1, 0)))
+    }
+
+    @Test
+    fun testFindMaxUnderBoundary() {
+        assertEquals(4, arraysAlgo.findMaxUnderBoundary(longArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 5))
+    }
+
+    @Test
+    fun testFindTopElements() {
+        assertEquals(listOf<Long>(30, 20, 10), arraysAlgo.findTopElements(longArrayOf(1, 2, 3, 4, 10, 20, 30), 3))
     }
 }
